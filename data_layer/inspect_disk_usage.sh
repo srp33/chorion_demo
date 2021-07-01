7a9e7cd0745483c6f7f919376eb4f7a4ae4eb4e3
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2021-07-01 13:38:57 -0600

    Added details about checking disk usage

--- data_layer/inspect_disk_usage.sh
+++ data_layer/inspect_disk_usage.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
 sudo du -s --block-size=1 /var/lib/docker/aufs
+
+# Before building data layer:
+# 111763021824	/var/lib/docker/aufs
+# After building data layer:
+# 111763038208	/var/lib/docker/aufs
+# Difference = 16384 = 2^14 (must be storage unit on this file system)
--- logic_layer/describe_data.py
+++ logic_layer/describe_data.py
@@ -4,4 +4,4 @@ with open(file_path) as data_file:
     lines = data_file.readlines()
 
     print(f"The {file_path} file has {len(lines)} of text.")
-    print(f"The first line has {len(lines[0])} elements.")
+    print(f"The first line has {len(lines[0])} elements...")
